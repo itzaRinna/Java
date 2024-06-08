a75b6e13ee6c318b4719d01a5c07a8c7f30c86ac
itzaRinna <dutchismahdick@gmail.com>
2024-06-08 08:42:44 +0700

    done init the project

---
+++ 1/src/App.java
@@ -0,0 +1,15 @@
+public class App{
+    public static void main(String[] args){
+        System.out.print("\"I love pizza\"\n");
+        System.out.println("It's really good");
+
+        // This is a comment
+
+        /*
+         * This
+         * is
+         * a
+         * comment
+         */
+    }
+}
